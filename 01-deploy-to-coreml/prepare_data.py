7a5587a08df1405876a68f3eed9ed9fb86fc5a9c
Ömür Özkir <oemuer.oezkir@gmail.com>
2018-07-01 23:15:46 +0200

    creates the labelled dataset based on the folder names

---
+++ 01-deploy-to-coreml/prepare_data.py
@@ -0,0 +1,15 @@
+import turicreate as tc
+import re
+import os
+
+
+def path_as_label(path):
+    label = re.search('(?<=raw/)(.+)?/.+$', path).group(1)
+    label = label.replace('_', ' ')
+    return label.title()
+
+
+img_path = fn = os.path.join(os.path.dirname(__file__), '../data/raw')
+data = tc.image_analysis.load_images(img_path, with_path=True)
+data['label'] = data['path'].apply(path_as_label)
+data.print_rows(num_rows=50)
