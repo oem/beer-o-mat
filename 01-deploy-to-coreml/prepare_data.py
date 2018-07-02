bce1882d0e64c93766a7d13b46123b952710952d
Ömür Özkir <oemuer.oezkir@gmail.com>
2018-07-02 18:18:15 +0200

    processes data and saves it as Sframe

--- 01-deploy-to-coreml/prepare_data.py
+++ 01-deploy-to-coreml/prepare_data.py
@@ -9,7 +9,9 @@ def path_as_label(path):
     return label.title()
 
 
-img_path = fn = os.path.join(os.path.dirname(__file__), '../data/raw')
+img_path = os.path.join(os.path.dirname(__file__), '../data/raw')
 data = tc.image_analysis.load_images(img_path, with_path=True)
 data['label'] = data['path'].apply(path_as_label)
 data.print_rows(num_rows=50)
+data.save(os.path.join(os.path.dirname(__file__),
+                       '../data/processed/beers.sframe'))
